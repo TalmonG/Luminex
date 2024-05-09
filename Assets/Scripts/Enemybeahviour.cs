6941e2646e988d6038152ae37265d768508ee1e8
SeemonThapa <seemonthapa7@gmail.com>
2024-05-09 18:38:49 +0100

    added enemy patrol

--- Assets/Scripts/Enemybeahviour.cs
+++ Assets/Scripts/Enemybeahviour.cs
@@ -7,6 +7,11 @@ public class Enemybeahviour : MonoBehaviour
     private GameObject Player;
     public float speed;
     private float dist;
+    public Transform[] patrolPoints;
+    public int patrolDestination;
+    public Transform playerTransform;
+    public bool isChasing;
+    public float chaseDistance;
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
