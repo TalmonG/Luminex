4a3950918dab9396848cfb112af5d1ef11dd5f85
Knights-wrath <a.knight.y@outlook.com>
2024-05-10 14:50:25 +0100

    shop

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class ShopScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Dictionary<string,int> items = new Dictionary<string,int>();
+    private GameObject Buttons;
+    private int Chrono = 10000;
+
     void Start()
+    {
+        items.Add("UpgradeToken", 100);
+        items.Add("PistolBullet", 5);
+        items.Add("RifleBullet", 15);
+        items.Add("JetpackFuel", 20);
+        items.Add("Bandage", 3);
+        items.Add("Medkit", 20);
+        items.Add("Syringe",15 );
+        items.Add("Steroids", 10);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void onClickShopButtons()
     {
-        
+        if (GameObject.FindWithTag("PistolBulletShopButton"))
+        {
+
+        }
+        else if (GameObject.FindWithTag("RifleBulletShopButton"))
+        {
+
+        }
+        else if (GameObject.FindWithTag("UpgradeToken"))
+        {
+
+        }
+        else if (GameObject.FindWithTag("RifleBulletShopButton"))
+        {
+
+        }
     }
 }
