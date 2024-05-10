aa5263eacec086480373e5b202335a1184846d3e
TalmonG <118438762+TalmonG@users.noreply.github.com>
2024-05-10 15:01:12 +0100

    SAVE

--- Assets/Scripts/DimensionManager.cs
+++ Assets/Scripts/DimensionManager.cs
@@ -4,32 +4,153 @@ using UnityEngine;
 
 public class DimensionManager : MonoBehaviour
 {
-    //referencing
-    public PlayerScript playerScript;
-    public GameObject normalDimension;
-    public GameObject invertedDimension;
+    /*//referencing
+    public PlayerStatsObj playerStatsObj;
+
+    // Tutorial
+    public GameObject normalDimensionTutorial;
+    public GameObject invertedDimensionTutorial;
+
+    // Level 1
+    public GameObject normalDimensionLvlOne;
+    public GameObject invertedDimensionLvlOne;
+
+    // Level 2
+    public GameObject normalDimensionLvlTwo;
+    public GameObject invertedDimensionLvlTwo;
+
+    // Level 3
+    public GameObject normalDimensionLvlThree;
+    public GameObject invertedDimensionLvlThree;
+
+    // Level 4
+    public GameObject normalDimensionLvlFour;
+    public GameObject invertedDimensionLvlFour;
 
     // Start is called before the first frame update
     void Start()
     {
-        normalDimension.SetActive(true);
-        invertedDimension.SetActive(false);
+        // Tutorial
+        normalDimensionTutorial.SetActive(true);
+        invertedDimensionTutorial.SetActive(false);
+
+        // Level 1
+        normalDimensionLvlOne.SetActive(true);
+        invertedDimensionLvlOne.SetActive(false);
+
+        // Level 2
+        normalDimensionLvlTwo.SetActive(true);
+        invertedDimensionLvlTwo.SetActive(false);
+
+        // Level 3
+        normalDimensionLvlThree.SetActive(true);
+        invertedDimensionLvlThree.SetActive(false);
+
+        // Level 4
+        normalDimensionLvlFour.SetActive(true);
+        invertedDimensionLvlFour.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerScript.isNormalDimension == true)
+        if (playerStatsObj.currentLevel == -6)
         {
-            normalDimension.SetActive(true);
-            invertedDimension.SetActive(false);
+            //
         }
-        else if (playerScript.isNormalDimension == false)
+        else if (playerStatsObj.currentLevel == -5)
         {
-            normalDimension.SetActive(false);
-            invertedDimension.SetActive(true);
+            //
+        }
+        else if (playerStatsObj.currentLevel == -4)
+        {
+            //
+        }
+        else if (playerStatsObj.currentLevel == -3)
+        {
+            //
+        }
+        else if (playerStatsObj.currentLevel == -2)
+        {
+            //
+        }
+        else if (playerStatsObj.currentLevel == -1)
+        {
+            //
+        }
+        // Tutorial
+        else if (playerStatsObj.currentLevel == 0)
+        {
+            if (playerStatsObj.isNormalDimension == true)
+            {
+                normalDimension.SetActive(true);
+                invertedDimension.SetActive(false);
+            }
+            else if (playerStatsObj.isNormalDimension == false)
+            {
+                normalDimension.SetActive(false);
+                invertedDimension.SetActive(true);
+            }
+        }
+        // Level 1
+        else if (playerStatsObj.currentLevel ==  1)
+        {
+            if (playerStatsObj.isNormalDimension == true)
+            {
+                normalDimension.SetActive(true);
+                invertedDimension.SetActive(false);
+            }
+            else if (playerStatsObj.isNormalDimension == false)
+            {
+                normalDimension.SetActive(false);
+                invertedDimension.SetActive(true);
+            }
+        }
+        // Level 2
+        else if (playerStatsObj.currentLevel == 2)
+        {
+            if (playerStatsObj.isNormalDimension == true)
+            {
+                normalDimension.SetActive(true);
+                invertedDimension.SetActive(false);
+            }
+            else if (playerStatsObj.isNormalDimension == false)
+            {
+                normalDimension.SetActive(false);
+                invertedDimension.SetActive(true);
+            }
+        }
+        // Level 3
+        else if (playerStatsObj.currentLevel == 3)
+        {
+            if (playerStatsObj.isNormalDimension == true)
+            {
+                normalDimension.SetActive(true);
+                invertedDimension.SetActive(false);
+            }
+            else if (playerStatsObj.isNormalDimension == false)
+            {
+                normalDimension.SetActive(false);
+                invertedDimension.SetActive(true);
+            }
+        }
+        // Level 4
+        else if (playerStatsObj.currentLevel == 4)
+        {
+            if (playerStatsObj.isNormalDimension == true)
+            {
+                normalDimension.SetActive(true);
+                invertedDimension.SetActive(false);
+            }
+            else if (playerStatsObj.isNormalDimension == false)
+            {
+                normalDimension.SetActive(false);
+                invertedDimension.SetActive(true);
+            }
         }
 
+        
 
-    }
+
+    }*/
 }
