6cc06ce2526a7571c56ef0cd61ff02ab70693bbc
FateKilerz <123807628+FateKilerz@users.noreply.github.com>
2024-05-10 19:46:04 +0100

    Finally Done

--- Assets/Scripts/Enemy Ai Script.cs
+++ Assets/Scripts/Enemy Ai Script.cs
@@ -13,9 +13,13 @@ public class EnemyAiScript : MonoBehaviour
     public int nextID;
     int idchangeValue = 1;
     public float speed = 2;
-   
-  
-
+    public int left_right = 1;
+    public bool isChasingPlayer=false;
+    public bool ReachedEdge=false;
+    public bool playerinrange;
+    public GameObject TargetCollider;
+    public GameObject HitCollider;
+    public GameObject[] Colliders;
 
     private float distance;
     bool seenplayer;
@@ -49,30 +53,62 @@ public class EnemyAiScript : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+
+        TargetCollider = Colliders[1];
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(HitCollider != null)
+        {
+            if (HitCollider.CompareTag("LeftCollider"))
+            {
+                TargetCollider = Colliders[1];
+            }
+            if (HitCollider.CompareTag("RightCollider"))
+            {
+                TargetCollider = Colliders[0];
+            }
+        }
+
+        //Debug.Log(HitCollider.gameObject.name);
+        
+        Debug.Log(playerinrange);
+
         float distance = Vector2.Distance(transform.position, player.transform.position);
 
-        if (distance < 4)
+        if (distance < 4&&playerinrange)
         {
             seenplayer = true;
-          
+           isChasingPlayer = true;
             
         }
 
-        
-       
-        if (seenplayer){
+        if (!playerinrange)
+        {
+            isChasingPlayer= false;
+        }
+
+        Debug.Log(TargetCollider.name);
+
+        if (isChasingPlayer){
             chaseplayer();
 
         }
-        else { MoveToNextPoint(); }
+        if (ReachedEdge)
+        {
+            patrol();
+           ReachedEdge = false;
+        }
+    
+        if (!isChasingPlayer) { patrol(); }
+
+       // else { MoveToNextPoint(); }
     }
     void MoveToNextPoint()
-    {
+    {/*
         Transform goalPoint = points[nextID];
         if (goalPoint.transform.position.x > transform.position.x)
             transform.localScale = new Vector3(-1, 1, 1);
@@ -87,8 +123,9 @@ public class EnemyAiScript : MonoBehaviour
                 idchangeValue = 1;
             nextID += idchangeValue;
         }
-        
+        */
     }
+
         
     
     void chaseplayer()
@@ -98,8 +135,27 @@ public class EnemyAiScript : MonoBehaviour
         //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         
         
-            transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        
         
     }
    
+
+    void patrol()
+    {
+        if (TargetCollider.CompareTag("RightCollider"))
+        {
+
+            
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (TargetCollider.CompareTag("LeftCollider"))
+        {
+
+            
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+    }
+
 }
--- Assets/Scripts/ground script.cs
+++ Assets/Scripts/ground script.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class groundscript : MonoBehaviour
 {
     bool floor;
-    EnemyPlatformscript juggernautscript;
+    EnemyAiScript juggernautscript;
     GameObject player;
     public float speed;
     bool seenplayer;
@@ -16,11 +16,8 @@ public class groundscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        juggernautscript = GameObject.FindGameObjectWithTag("Juggernaut").transform.GetComponent<EnemyPlatformscript>();
-
-        player = GameObject.FindGameObjectWithTag("Player");
-      
-
+        juggernautscript = GameObject.FindGameObjectWithTag("Juggernaut").transform.GetComponent<EnemyAiScript>();
+ 
     }
     
     
@@ -32,67 +29,31 @@ public class groundscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+       
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision != null)
-        {
-            if (collision.CompareTag("ground"))
-            {
-                floor = true;   
-
-            }
+        {         
 
             if (collision.CompareTag("Juggernaut"))
             {
-                Debug.Log("dist");
 
+            
+                Debug.Log("hitedge");
+
+                juggernautscript.HitCollider = this.gameObject;
+                juggernautscript.isChasingPlayer = false;
+                juggernautscript.ReachedEdge = true;
                 juggernautscript.left_right *= -1;
 
             }
             
         }
-        float distance = Vector2.Distance(transform.position, player.transform.position);
 
-        if (distance < 4)
-        {
-            seenplayer = true;
-
-
-        }
-
-
-
-        if (seenplayer)
-        {
-            chaseplayer();
-
-        }
-        else { }
         
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision != null)
-
-        {
-            if (collision.gameObject.CompareTag("ground"))
-            {
-                floor = true;
-
-            }
-
-        }
-    }
-    void chaseplayer()
-    {
-
-        Vector2 direction = player.transform.position - transform.position;
-        //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-
-
-        transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-
     }
 
 
