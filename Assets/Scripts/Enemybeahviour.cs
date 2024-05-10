40ad08829f218b119dd52e80efd5fed5d55ebe4b
SeemonThapa <seemonthapa7@gmail.com>
2024-05-10 17:37:32 +0100

    modified enemy behaviour

--- Assets/Scripts/Enemybeahviour.cs
+++ Assets/Scripts/Enemybeahviour.cs
@@ -12,6 +12,11 @@ public class Enemybeahviour : MonoBehaviour
     public Transform playerTransform;
     public bool isChasing;
     public float chaseDistance;
+    public bool onground;
+
+
+    public object PlayerTransformation { get; private set; }
+
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
@@ -19,7 +24,50 @@ public class Enemybeahviour : MonoBehaviour
 
     private void Update()
     {
-      
+
+        if (isChasing)
+        {
+            if(transform.position.x > Player.transform.position.x) 
+            {
+                transform.localScale = new Vector3(-1, 1, 1); 
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (transform.position.x < Player.transform.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
+        
+        
+            if (Vector3.Distance(transform.position, Player.transform.position) < chaseDistance) 
+            {
+            isChasing = true;
+            }
+
+        if (isChasing == false)
+        {
+            if (patrolDestination == 0)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, patrolPoints[0].position) < .3f)
+                {
+
+                    transform.localScale = new Vector3(1, 1, 1);
+                    patrolDestination = 1;
+                }
+            }
+            if (patrolDestination == 1)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, patrolPoints[1].position) < .3f)
+                {
+
+                    transform.localScale = new Vector3(-1, 1, 1);
+                    patrolDestination = 0;
+                }
+            }
+        }
     }
 
 }
